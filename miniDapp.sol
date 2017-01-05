1ef138c1e0df240463591a4d6b7d9e21b6b117e9
maxxflyer <maxxflyer@gmail.com>
2017-01-05 19:10:19 +0100

    Update miniDapp.sol

--- miniDapp.sol
+++ miniDapp.sol
@@ -1,35 +1,24 @@
 contract miniDapp{
-//use this variable in ALL you contracts! STANDARD
-address public owner;
 
-//a basic storage
-string simpleString;
+   //use this variable in ALL your contracts! STANDARD
+   address public owner;
 
-//an indexed storage
-mapping(uint => string)mainMemory;
+   //a basic storage
+   string simpleString;
 
-function miniDapp(){
-   owner=msg.sender;
-}
+   function miniDapp(){
+      owner=msg.sender;
+   }
 
-function setsimpleString(string s) returns(bool){
-   if(msg.sender!=owner)throw;
-   simpleString=s;
-   return true;
-}
+   function setsimpleString(string s) returns(bool){
+      if(msg.sender!=owner)throw;
+      simpleString=s;
+      return true;
+   }
 
-function getsimpleString()constant returns(string){
-   return simpleString;
-}
+   function getsimpleString()constant returns(string){
+      return simpleString;
+   }
 
-function setMemory(uint index,string s) returns(bool){
-   if(msg.sender!=owner)throw;
-   mainMemory[index]=s;
-   return true;
-}
-
-function getString(uint index)constant returns(string){
-   return mainMemory[index];
-}
 
 }
