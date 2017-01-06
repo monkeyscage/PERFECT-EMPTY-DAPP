3a98671e424c6aeb4b88dde27342b42fe7c29d40
maxxflyer <maxxflyer@gmail.com>
2017-01-06 03:54:36 +0100

    Update miniDappInterface.js

--- js/miniDappInterface.js
+++ js/miniDappInterface.js
@@ -1,8 +1,8 @@
 
 contracts["miniDapp"] = {
 	interface: [ { "constant": true, "inputs": [], "name": "getsimpleString", "outputs": [ { "name": "", "type": "string", "value": "" } ], "payable": false, "type": "function" }, { "constant": false, "inputs": [ { "name": "s", "type": "string" } ], "name": "setsimpleString", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address", "value": "" } ], "payable": false, "type": "function" }, { "inputs": [], "payable": false, "type": "constructor" } ],
-	address: "0xe6d8e335373EA7Bae6f10dF4190e63a1e8732F36"
+	address: "0x039517104A0422522A2aE73134F06973C7419353"
 };
 contracts["miniDapp"].contractClass = web3.eth.contract(contracts["miniDapp"].interface);
 contracts["miniDapp"].contract = contracts["miniDapp"].contractClass.at(contracts["miniDapp"].address);
-if (!ctrAddresses["miniDapp"]){ ctrAddresses["miniDapp"] = [] }ctrAddresses["miniDapp"].push("0xe6d8e335373EA7Bae6f10dF4190e63a1e8732F36");
+if (!ctrAddresses["miniDapp"]){ ctrAddresses["miniDapp"] = [] }ctrAddresses["miniDapp"].push("0x039517104A0422522A2aE73134F06973C7419353");
